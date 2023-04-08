9713006d3d0d37fe00d27280e78aa038d212c8b8
Michael Aguas <aguasbonillamichael@gmail.com>
2023-04-07 19:47:26 -0500

    interactive mobile menu and media quaries

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 const navEmail = document.querySelector(".navbar-email")
 const deskMenu = document.querySelector(".desktop-menu")
+const mobMenu = document.querySelector(".mobile-menu")
+const hamIcoMenu = document.querySelector(".menu")
 
 navEmail.addEventListener("click", toggleDesktopMenu) 
+hamIcoMenu.addEventListener("click", toggleMobileMenu)
 
 function toggleDesktopMenu() { 
     deskMenu.classList.toggle("inactive")
+}
+
+function toggleMobileMenu() { 
+    mobMenu.classList.toggle("inactive")
 }
